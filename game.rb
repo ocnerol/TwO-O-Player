79c3ef84ab4f4e7cc772c355b4a632a109cb071f
Lorenço <lorenco.alvareznavarrete@gmail.com>
2022-01-31 20:37:51 -0700

    Design new_turn method

--- game.rb
+++ game.rb
@@ -4,6 +4,7 @@ class Game
     @player_1 = player_1
     @player_2 = player_2
     @in_session? = true
+    @last_player = @player_1
   end
   
   def scoreboard
@@ -13,6 +14,12 @@ class Game
   def final_report
     winner = @player_1.lives > @player_2.lives ? @player_1 : @player_2
     puts "#{winner.name} wins with a score of #{winner.lives}/3"
+  end
+
+  def new_turn
+    player_in_next_turn = @last_player == @player_1 ? @player_2 : @player_1
+    @last_player = player_in_next_turn
+    Turn(player_in_next_turn)
   end
 
 end
