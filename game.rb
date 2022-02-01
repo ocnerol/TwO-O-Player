cfd04cc8180896b7e4399a4fc5cc548a5f63c77b
Lorenço <lorenco.alvareznavarrete@gmail.com>
2022-01-31 20:32:23 -0700

    Design scoreboard and final_report methods

--- game.rb
+++ game.rb
@@ -6,5 +6,13 @@ class Game
     @in_session? = true
   end
   
+  def scoreboard
+    puts "P1: #{@player_1.lives}/3 vs P2: #{@player_2.lives}/3"
+  end
+
+  def final_report
+    winner = @player_1.lives > @player_2.lives ? @player_1 : @player_2
+    puts "#{winner.name} wins with a score of #{winner.lives}/3"
+  end
 
 end
